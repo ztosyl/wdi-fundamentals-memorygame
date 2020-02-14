cc60188905b1c406ee21cd09757ce3ea493e8c29
Zambeta Tsapos <ztsapos@Gmail.com>
2020-02-13 21:31:24 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,5 +1,11 @@
-let cardOne = "king";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "queen";
-console.log("User flipped " + cardThree);
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+if (cardsInPlay.length === 2){
+	cardsInPlay[0] === cardsInPlay[1] ? alert("You found a match!") : alert("Sorry, try again.");
+}
